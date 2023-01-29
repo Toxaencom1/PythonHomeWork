10def45a80dc81f4ed4e96e0386a91a3bafd1ef1
Toxaencom1 <Toxaencom@gmail.com>
2023-01-29 15:31:14 +0600

    fix some, for DZ9

--- DZ9/handlers.py
+++ DZ9/handlers.py
@@ -17,6 +17,7 @@ RULES = """ Правила игры конфеты: Для начала уста
 
 @dp.message_handler(commands=['calc'])
 async def phrase_calc_start(message: types.Message):
+    print(message.text)
     if ':' in message.text.split()[1]:
         temp = message.text.split()[1].replace(':', '/')
     else:
@@ -27,6 +28,7 @@ async def phrase_calc_start(message: types.Message):
 
 @dp.message_handler(commands=['start'])
 async def mes_start(message: types.Message):
+    print(message.text)
     end()  # Обнуляет Total и Coin
     await message.answer(f'Привет {message.from_user.first_name} Мы будем играть с тобой в конфетки\n')
     time.sleep(0.3)
@@ -38,16 +40,18 @@ async def mes_start(message: types.Message):
 
 @dp.message_handler(commands=['help'])
 async def help_start(message: types.Message):
+    print(message.text)
     await message.answer(f'Приветствую {message.from_user.first_name}!')
     time.sleep(0.5)
     await message.answer(f'Для просмотра инструкций к игре введите /start\n'
                          f'Так же можно закончить игру с помощью /end\n'
-                         f'и если уже очень хочется подсмотреть сколько осталось на кону - /get_total'
+                         f'и если уже очень хочется подсмотреть сколько осталось на кону - /get_total\n'
                          f'А еще можно посчитать в калькуляторе выражений - /calc')
 
 
 @dp.message_handler(commands=['set'])
 async def set_total(message: types.Message):
+    print(message.text)
     global total
     count = int(message.text.split()[1])
     total = count
@@ -58,6 +62,7 @@ async def set_total(message: types.Message):
 
 @dp.message_handler(commands=['set_150'])
 async def set_total_150(message: types.Message):
+    print(message.text)
     global total
     total = 150
     await message.answer(f'Установленно количество конфет = {total}')
@@ -67,18 +72,21 @@ async def set_total_150(message: types.Message):
 
 @dp.message_handler(commands=['end'])
 async def end_game(message: types.Message):
+    print(message.text)
     end()
     await message.answer(f'Игра завершена, или можно начать сначала, нажми /start')
 
 
 @dp.message_handler(commands=['get_total'])
 async def get_total_candys(message: types.Message):
+    print(message.text)
     await message.answer(f'Вот хитрец!!!! ладно, на кону осталось {get_total()} конфет.')
     await message.delete()
 
 
 @dp.message_handler(commands=['Orel'])
 async def coin_O_choice(message: types.Message):
+    print(message.text)
     global total
     global coin
     if total != 0:
@@ -108,6 +116,7 @@ async def coin_O_choice(message: types.Message):
 
 @dp.message_handler(commands=['Reshka'])
 async def coin_P_choice(message: types.Message):
+    print(message.text)
     global total
     global coin
     if total != 0:
@@ -143,6 +152,7 @@ async def mes_hi(message: types.Message):
 
 @dp.message_handler()
 async def mes_all(message: types.Message):
+    print(message.text)
     if message.text.isdigit():
         global total
         global coin
@@ -166,7 +176,7 @@ async def mes_all(message: types.Message):
                     bot = bot_turn()
                     mes = win(bot, 'Бот', player_name)
                     await message.answer(mes)
-        # await message.answer(f'Осталось конфет {get_total()}')
+        await message.answer(f'Осталось конфет {get_total()}')
     else:
         await message.answer(f'{message.from_user.full_name}, для того чтобы взять конфеты с кона '
                              f'введи количество без букв"')
